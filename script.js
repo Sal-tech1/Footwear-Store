ad763a7b356619d128dbfb7621d0db8d8f310754
Sal-tech1 <njorogesalem@gmail.com>
2025-01-20 20:00:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// Select all "Add to Cart" buttons
+// JavaScript to add functionality for "Add to Cart" buttons
+
+// Function to handle adding products to the cart
+function addToCart(event) {
+    const product = event.target.dataset.product;
+    alert(`${product} added to cart!`);
+}
+
+// Get all "Add to Cart" buttons
 const addToCartButtons = document.querySelectorAll('.add-to-cart');
 
-// Add event listeners to "Add to Cart" buttons
+// Add event listener to each button
 addToCartButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        // Get the product name from the data-product attribute
-        const productName = button.getAttribute('data-product');
-
-        // Show a confirmation message
-        alert(`${productName} added to cart!`);
-    });
+    button.addEventListener('click', addToCart);
 });
